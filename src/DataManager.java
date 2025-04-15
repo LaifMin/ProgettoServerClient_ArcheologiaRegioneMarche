6a5ae391c3b61c2a25824539b94d79ee80dbdd59
Laif <126174464+LaifMin@users.noreply.github.com>
2025-04-15 13:40:22 +0200

    Terzo commit: aggiunta comandi per client

--- src/DataManager.java
+++ src/DataManager.java
@@ -1,3 +1,4 @@
+package BusinessLogic;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -5,15 +6,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-public class DataManager  {
 
-    private ArrayList<Data> dataStructure = new ArrayList<>(); 
+public class DataManager {
+
+
+    private ArrayList<Data> dataStructure = new ArrayList<>();
     private static DataManager instance = null; // Singleton instance
-    private static final String FILE_NAME = "../Archeologia---Storia-e-Tradizioni---Regione-Marche.csv";
-    private static final String FILE_PATH = new File(FILE_NAME).getAbsolutePath(); 
-                                                                                                          
-                                                                                                         
+    private static final String FILE_NAME = "Archeologia---Storia-e-Tradizioni---Regione-Marche.csv";
+    private static final String FILE_PATH = new File(FILE_NAME).getAbsolutePath();
+
+
     private boolean isDataLoaded = false;
+
 
     private DataManager() {
         try {
@@ -23,11 +27,14 @@ public class DataManager  {
         }
         isDataLoaded = true;
 
+
     }
+
 
     public boolean isDataLoaded() { // Check if data is loaded
         return isDataLoaded;
     }
+
 
     public static synchronized DataManager getInstance() { // Using syncronized to ensure thread safety
         if (instance == null) {
@@ -36,9 +43,11 @@ public class DataManager  {
         return instance;
     }
 
+
     public int returnSize() { // Return the size of the data structure
         return dataStructure.size();
     }
+
 
     private void loadDataFromCSV() throws IOException, LineExeption {
         dataStructure.clear();
@@ -48,8 +57,9 @@ public class DataManager  {
             reader.readLine(); // field titles
             while ((linea = reader.readLine()) != null) {
                 lineNumber++;
-                String[] dati = splitCSVLine(linea); 
-    
+                String[] dati = splitCSVLine(linea);
+
+
                 if (dati.length == 13 || dati.length == 12) {
                     String urlImmagine = dati[0];
                     String didascalia = dati[1];
@@ -63,52 +73,263 @@ public class DataManager  {
                     String urlSitoWeb = dati[9];
                     String latitudine = dati[10];
                     String longitudine = dati[11];
-                    String orariDiApertura = (dati.length == 13) ? dati[12] : "";  // if opening hours is empty then set it to ""
-    
+                    String orariDiApertura = (dati.length == 13) ? dati[12] : ""; // if opening hours is empty then set
+                                                                                  // it to ""
+
+
                     Data data = new Data(
-                        urlImmagine, didascalia, tipologiaPDI, denominazione, comune,
-                        indirizzo, civico, telefono, email, urlSitoWeb, latitudine,
-                        longitudine, orariDiApertura
-                    );
+                            urlImmagine, didascalia, tipologiaPDI, denominazione, comune,
+                            indirizzo, civico, telefono, email, urlSitoWeb, latitudine,
+                            longitudine, orariDiApertura);
                     dataStructure.add(data);
                 } else {
-                    throw new LineExeption(lineNumber); 
+                    throw new LineExeption(lineNumber);
                 }
             }
         }
     }
+
 
     private String[] splitCSVLine(String line) {
         List<String> fields = new ArrayList<>();
         StringBuilder currentField = new StringBuilder();
         boolean inQuotes = false;
 
+
         for (char c : line.toCharArray()) {
             if (c == '"') {
-           
+
+
                 inQuotes = !inQuotes;
             } else if (c == ';' && !inQuotes) {
-               
+
+
                 fields.add(currentField.toString().trim());
-                currentField.setLength(0); 
+                currentField.setLength(0);
             } else {
                 currentField.append(c);
             }
         }
-       
+
+
         fields.add(currentField.toString().trim());
+
 
         return fields.toArray(new String[0]);
     }
 
-    /**
-     * @param int index
-     * @return Data
-     */
-    public Data getRow(int index) {
+
+    public Data getRow(int index) { // Get a row of data using GET_ROW <index>
         if (index < 0 || index >= dataStructure.size())
             return null;
         return dataStructure.get(index);
     }
+
+
+    public ArrayList<Data> searchWDenominazione(String denominazione) { // Search by name using SEARCH -n <name>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getDenominazione().equalsIgnoreCase(denominazione)) {
+                tmp.add(data);
+            }
+        }
+
+
+        return tmp;
+    }
+
+
+    public ArrayList<Data> searchWComune(String comune) { // Search by city using SEARCH -c <city>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getComune().equalsIgnoreCase(comune)) {
+                tmp.add(data);
+            }
+        }
+        return tmp;
+    }
+
+
+    public ArrayList<Data> searchWTipologia(String tipologia) { // Search by type using SEARCH -t <type>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getTipologiaPDI().equalsIgnoreCase(tipologia)) {
+                return tmp;
+            }
+        }
+        return tmp;
+    }
+
+
+    public Data searchWIndirizzo(String indirizzo) { // Search by address using SEARCH -a <address>
+        for (Data data : dataStructure) {
+            if (data.getIndirizzo().equalsIgnoreCase(indirizzo)) {
+                return data;
+            }
+        }
+        return null;
+    }
+
+
+    public ArrayList<Data> searchWDenominazioneParziale(String denominazione) { // Search by partial name using SEARCH
+                                                                                // --n <name>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getDenominazione().toLowerCase().contains(denominazione.toLowerCase())) {
+                tmp.add(data);
+            }
+        }
+        return tmp;
+    }
+
+
+    public ArrayList<Data> searchWComuneParziale(String comune) { // Search by partial city using SEARCH --c <city>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getComune().toLowerCase().contains(comune.toLowerCase())) {
+                tmp.add(data);
+            }
+        }
+        return tmp; // Return null if not found
+    }
+
+
+    public ArrayList<Data> searchWTipologiaParziale(String tipologia) { // Search by partial type using SEARCH --t
+                                                                        // <type>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getTipologiaPDI().toLowerCase().contains(tipologia.toLowerCase())) {
+                tmp.add(data);
+            }
+        }
+        return tmp; // Return null if not found
+    }
+
+
+    public ArrayList<Data> searchWIndirizzoParziale(String indirizzo) { // Search by partial address using SEARCH --a
+                                                                        // <address>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getIndirizzo().toLowerCase().contains(indirizzo.toLowerCase())) {
+                tmp.add(data);
+            }
+        }
+        return tmp; // Return null if not found
+    }
+
+
+    public ArrayList<Data> sortDataStructure() { // Sort the data structure by name using SORT -n
+
+
+        ArrayList<Data> sortedData = new ArrayList<>(dataStructure);
+        sortedData.sort((data1, data2) -> {
+            String denominazione1 = data1.getDenominazione().toLowerCase();
+            String denominazione2 = data2.getDenominazione().toLowerCase();
+            return denominazione1.compareTo(denominazione2);
+        });
+        return sortedData;
+    }
+
+
+    public ArrayList<Data> sortDataStructureByComune() { // Sort the data structure by city using SORT -c
+
+
+        ArrayList<Data> sortedData = new ArrayList<>(dataStructure);
+        sortedData.sort((data1, data2) -> {
+            String comune1 = data1.getComune().toLowerCase();
+            String comune2 = data2.getComune().toLowerCase();
+            return comune1.compareTo(comune2);
+        });
+        return sortedData;
+    }
+
+
+    public ArrayList<Data> sortDataStructureByTipologia() { // Sort the data structure by type using SORT -t
+
+
+        ArrayList<Data> sortedData = new ArrayList<>(dataStructure);
+        sortedData.sort((data1, data2) -> {
+            String tipologia1 = data1.getTipologiaPDI().toLowerCase();
+            String tipologia2 = data2.getTipologiaPDI().toLowerCase();
+            return tipologia1.compareTo(tipologia2);
+        });
+        return sortedData;
+    }
+
+
+    public ArrayList<Data> sortDataStructureByIndirizzo() { // Sort the data structure by address using SORT -a
+
+
+        ArrayList<Data> sortedData = new ArrayList<>(dataStructure);
+        sortedData.sort((data1, data2) -> {
+            String indirizzo1 = data1.getIndirizzo().toLowerCase();
+            String indirizzo2 = data2.getIndirizzo().toLowerCase();
+            return indirizzo1.compareTo(indirizzo2);
+        });
+        return sortedData;
+    }
+
+
+   
+    public ArrayList<Data> searchByOpenDay(String day) { // Search by open day using SEARCH --o <day>
+        ArrayList<Data> tmp = new ArrayList<>();
+        for (Data data : dataStructure) {
+            if (data.getOrariDiApertura().toLowerCase().contains(day.toLowerCase())) {
+                tmp.add(data);
+            }
+        }
+        return tmp;
+    }
+
+
+
+
+    public int getNumberByTipologia(String tipologia){ // Get the number of types using COUNT -t <type>
+        int count = 0;
+        for(Data data : dataStructure){
+            if(data.getTipologiaPDI().toLowerCase().equals(tipologia.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
+       
+    }
+
+
+   
+    public int getNumberByComune(String comune){ // Get the number of cities using COUNT -c <city>
+        int count = 0;
+        for(Data data : dataStructure){
+            if(data.getComune().toLowerCase().equals(comune.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public int getNumberByTipologiaParziale(String tipologia){ // Get the number of partial types using COUNT --t <type>
+        int count = 0;
+        for(Data data : dataStructure){
+            if(data.getTipologiaPDI().toLowerCase().contains(tipologia.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+    public int getNumberByComuneParziale(String comune){ // Get the number of partial cities using COUNT --c <city>
+        int count = 0;
+        for(Data data : dataStructure){
+            if(data.getComune().toLowerCase().contains(comune.toLowerCase())){
+                count++;
+            }
+        }
+        return count;
+    }
+
+
+
 
 }
